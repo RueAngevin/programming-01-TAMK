47bdd564e7347198c65968eff1546b7915d5fb41
RueAngevin <rueangevin@gmail.com>
2024-10-03 11:02:18 +0300

    file moved

---
+++ week_40/e01/App.java
@@ -0,0 +1,21 @@
+package e01;
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        String name = scanner.nextLine();
+
+        if(name == "louis") {
+            System.out.println("Are you French?");
+        } else {
+            System.out.println("You have a great name");
+        }
+
+        scanner.close();
+    }
+}
